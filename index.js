f4eb5932801c1166ea88276736b023ad515a5cba
roland.veres <roland.veres@bca.com>
2024-09-09 22:09:55 +0300

    commit

--- index.js
+++ index.js
@@ -1,93 +1,115 @@
-const addTodo = {
-    type:'ADD_TODO',
-    todo:{
-        id:0,
-        name:'Learn Redux',
-        complete:false
-    }
-};
-
-const removeTodo = {
-    type:'REMOVE_TODO',
-    id:0 
-};
-
-const toggleTodo = {
-    type:'TOGGLE_TODO',
-    id:0
-};
-
-
-const removeGoal = {
-    type:'REMOVE_GOAL',
-    id:0
-}
-
-const  addGoal = {
-    type:'Add_GOAL',
-    id:0 
-};
-
-
-//Reducer function 
-function todos( state=[], action) {
-    switch(action.type) {
-        case 'ADD_TODO':  return state.concat([action.todo])
-        case 'REMOVE_TODO':  return state.filter(todo=> todo.id !== action.id);
-        case 'UPDATE_TODO':  return state.filter(todo=> todo.id!== action.id ? todo: action.todo);
-        case 'TOGGLE_TODO':  return  state.map(todo=> todo.id !== action.id? todo: {...todo,complete:!todo.complete});
-        default: return state;
-    }
-}
-
 function createStore(reducer) {
-    // 1. the state 
-     let state;
+  // 1. the state
+  let state;
 
-     let listeners = [];
+  let listeners = [];
 
-    // 2. get the state 
-    const getState = ()=>state;
+  // 2. get the state
+  const getState = () => state;
 
-    // 3. Listen to changes on the state, return a method to unsubscribe 
-    const subscribe = (listener)=> {
-        listeners.push(listener);
+  // 3. Listen to changes on the state, return a method to unsubscribe
+  const subscribe = (listener) => {
+    listeners.push(listener);
 
-        return ()=> {
-            listeners = listeners.filter(l=>l!==listener);
-        }
-    }
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
 
-    // 4. update the state 
-    const dispatch = (action) => {
-        //call todos
-        state = reducer(state,action)
+  // 4. update the state
+  const dispatch = (action) => {
+    //call todos
+    state = reducer(state, action);
+    console.log(state);
 
-        // loop over listeners and invoke them 
-        listeners.forEach(l=>l(state));
-    }
+    // loop over listeners and invoke them
+    listeners.forEach((l) => l(state));
+  };
 
-    // 5. return the object 
-    return {
-        getState,
-        subscribe,
-        dispatch
-    }
+  // 5. return the object
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
 }
 
-const store = createStore(todos);
+const ACTIONS = {
+  TODOS: {
+    ADD_TODO: "ADD_TODO",
+    REMOVE_TODO: "REMOVE_TODO",
+    UPDATE_TODO: "UPDATE_TODO",
+    TOGGLE_TODO: "TOGGLE_TODO",
+  },
+  GOALS: {
+    ADD_GOAL: "ADD_GOAL",
+    REMOVE_GOAL: "REMOVE_GOAL",
+  },
+};
 
-const unsubscribe1 = store.subscribe((newValue)=>console.log("listener number 1 receives new value",newValue));
-const unsubscribe2 = store.subscribe((newValue)=>console.log("listener number 2 receives new value",newValue));
-const unsubscribe3 = store.subscribe((newValue)=>console.log("listener number 3 receives new value",newValue));
+const todoActionCreators = {
+  add: (todo) => ({ type: ACTIONS.TODOS.ADD_TODO, todo }),
+  remove: (id) => ({ type: ACTIONS.TODOS.REMOVE_TODO, id }),
+  update: (id, todo) => ({ type: ACTIONS.TODOS.UPDATE_TODO, id, todo }),
+  toggle: (id) => ({ type: ACTIONS.TODOS.TOGGLE_TODO, id }),
+};
 
-store.dispatch(addTodo);
-unsubscribe1();
-store.dispatch(addTodo);
-unsubscribe2();
-store.dispatch(addTodo);
-unsubscribe3();
-store.dispatch(addTodo);
+function addTodoAction(todo) {
+  return {
+    type: ACTIONS.TODOS.ADD_TODO,
+    todo,
+  };
+}
 
+function goals(state = [], action) {
+  switch (action.type) {
+    case ACTIONS.GOALS.ADD_GOAL:
+      return state.concat([action.goal]);
+    case ACTIONS.GOALS.REMOVE_GOAL:
+      return state.filter((todo) => todo.id !== action.id);
+    default:
+      return state;
+  }
+}
 
+//Reducer function
+function todos(state = [], action) {
+  switch (action.type) {
+    case ACTIONS.TODOS.ADD_TODO:
+      return state.concat([action.todo]);
+    case ACTIONS.TODOS.REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+    case ACTIONS.TODOS.UPDATE_TODO:
+      return state.filter((todo) =>
+        todo.id !== action.id ? todo : action.todo
+      );
+    case ACTIONS.TODOS.TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id !== action.id ? todo : { ...todo, complete: !todo.complete }
+      );
+    default:
+      return state;
+  }
+}
 
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  };
+}
+
+const store = createStore(app);
+
+const unsubscribe1 = store.subscribe((newValue) =>
+  console.log("listener number 1 receives new value", newValue)
+);
+//const unsubscribe2 = store.subscribe((newValue)=>console.log("listener number 2 receives new value",newValue));
+//const unsubscribe3 = store.subscribe((newValue)=>console.log("listener number 3 receives new value",newValue));
+
+//store.dispatch({ type: "ADD_GOAL", goal: { id: 1, name: "something" } });
+
+store.dispatch(
+  todoActionCreators.add({ id: 1, name: "something", complete: false })
+);
+store.dispatch(todoActionCreators.toggle(1));
